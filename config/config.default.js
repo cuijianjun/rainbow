b14b0be074e2cdae19d8c699274e3bbccbd65607
cuijianjun <cuijianjun@xiaoyezi.com>
2018-12-15 00:53:04 +0800

    mysql finish connect

--- config/config.default.js
+++ config/config.default.js
@@ -13,7 +13,7 @@ module.exports = appInfo => {
     dialect: 'mysql',
     host: '127.0.0.1',
     port: 3306,
-    database: 'egg-sequelize-doc-default',
+    database: 'database_development',
   };
 
   config.logger = {
