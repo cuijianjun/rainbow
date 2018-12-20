fb6a78de50b043d0823641fd70446573805fa577
cuijianjun <cuijianjun@xiaoyezi.com>
2018-12-20 23:48:45 +0800

    sequelize ready

---
+++ config/config.prod.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+  config.cluster = {
+    listen: {
+      port: 3001,
+      hostname: '127.0.0.1',
+      // path: '/var/run/egg.sock',
+    }
+  };
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'database_production',
+  };
+  return config;
+};
