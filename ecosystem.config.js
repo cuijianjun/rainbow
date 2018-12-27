b85dc99207b326896af42f583f0e56887739be30
cuijianjun <cuijianjun@xiaoyezi.com>
2018-12-27 19:12:16 +0800

    deploy by pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps : [{
+    name      : 'rainbow',
+    script    : 'server.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'cuijianjun',
+      host : '123.56.24.253',
+      ref  : 'origin/master',
+      repo : 'git@github.com:cuijianjun/rainbow.git',
+      path : '/work/rainbow/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'cuijianjun',
+      host : '123.56.24.253',
+      ref  : 'origin/dev',
+      repo : 'git@github.com:cuijianjun/rainbow.git',
+      path : '/work/rainbow/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
