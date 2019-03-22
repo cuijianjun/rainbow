7eedc5ba70a91c65aec207bf8107b841fed450fd
Frank <376247934@qq.com>
2019-03-22 16:56:01 +0800

    AIPL-581: [fix] add auth

--- app/router.js
+++ app/router.js
@@ -28,10 +28,10 @@ module.exports = app => {
   // product_list
   router.post('ProductList', '/api/product_list/list', 'productlist.index'); // ok
   router.get('ProductList', '/api/product_list/detail/:product_id/:user_id', 'productlist.show'); // ok
-  router.post('ProductList', '/api/product_list/create', 'productlist.create');// ok
-  router.post('ProductList', '/api/product_list/update', 'productlist.update');
-  router.get('ProductList', '/api/product_list/delete/:id', 'productlist.destroy');
-  router.get('ProductList', '/api/product_list/updateTime/:id', 'productlist.updateTime');
+  router.post('ProductList', '/api/product_list/create',auth, 'productlist.create');// ok
+  router.post('ProductList', '/api/product_list/update',auth, 'productlist.update');
+  router.get('ProductList', '/api/product_list/delete/:id',auth, 'productlist.destroy');
+  router.get('ProductList', '/api/product_list/updateTime/:id',auth, 'productlist.updateTime');
 
   // 微信支付
   router.post('Pay', '/api/pay/unifiedOrder', 'pay.unifiedOrder'); // 统一下单接口
