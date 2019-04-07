1b0880d0b3fbbd855fa522d5dc8689c6f686d938
Frank <376247934@qq.com>
2019-04-07 20:44:21 +0800

    AIPL-748: [feature] add tel for user

--- app/model/user.js
+++ app/model/user.js
@@ -10,15 +10,13 @@ module.exports = app => {
     gender: {type: STRING(255), defaultValue: ''}, // 性别
     country: {type: STRING(255), defaultValue: ''}, // 国家
     city: {type: STRING(255), defaultValue: ''}, // 城市
-    tel: {type: INTEGER, defaultValue: ''},// 电话
+    tel: INTEGER,// 电话
     updated_at: DATE, // 更新时间
   }, {
     // 是否需要增加createdAt、updatedAt、deletedAt字段
     timestamps: true,
-
     // 不需要createdAt字段
     createdAt: false,
-
     // 将deletedAt字段改名
     // 同时需要设置paranoid为true（此种模式下，删除数据时不会进行物理删除，而是设置deletedAt为当前时间
     deletedAt: 'dTime',
