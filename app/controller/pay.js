509f7dae6e8813176a4e614feb72e12a0698c4d7
Frank <376247934@qq.com>
2019-04-14 01:04:18 +0800

    TOERP-1865: [fix] modify notify

--- app/controller/pay.js
+++ app/controller/pay.js
@@ -98,7 +98,7 @@ class PayController extends Controller {
         return_msg: 'OK',
       };
       console.log(order.dataValues.total_price, 'order.dataValues.total_price');
-      if (order.dataValues.total_price === return_data.total_fee) {
+      if (order.dataValues.total_price * 100 === return_data.total_fee) {
         console.log(order.dataValues.status, 'order.dataValues.status');
         if (order.dataValues.status === 1) {
           order.dataValues.status = 2;
