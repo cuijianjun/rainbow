2a33b9550e3015a1e631927ef24d4b79bb535a3b
Frank <376247934@qq.com>
2019-04-14 12:30:43 +0800

    TOERP-1865: [fix] modify findOne

--- app/service/hotSearch.js
+++ app/service/hotSearch.js
@@ -13,7 +13,7 @@ class HotSearch extends Service {
   }
 
   async update({ id = 0, updates }) {
-    const HotSearch = await this.ctx.model.HotSearch.findOne(id);
+    const HotSearch = await this.ctx.model.HotSearch.findOne({id});
     if (!HotSearch) {
       this.ctx.throw(404, 'HotSearch not found');
     }
@@ -21,7 +21,7 @@ class HotSearch extends Service {
   }
 
   async del(id = 0) {
-    const HotSearch = await this.ctx.model.HotSearch.findOne(id);
+    const HotSearch = await this.ctx.model.HotSearch.findOne({id});
     if (!HotSearch) {
       this.ctx.throw(404, 'HotSearch not found');
     }
